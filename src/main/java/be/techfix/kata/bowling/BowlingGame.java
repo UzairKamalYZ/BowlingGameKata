9eaf376990a1d7d58b95f9e6ac0ac9697240eecb
ukamal <ukamal@isabelgroup.eu>
2021-12-09 13:57:37 +0100

    fix problem with last frame.

--- src/main/java/be/techfix/kata/bowling/BowlingGame.java
+++ src/main/java/be/techfix/kata/bowling/BowlingGame.java
@@ -1,28 +1,36 @@
 package be.techfix.kata.bowling;
 
+import java.util.Arrays;
+
 public class BowlingGame {
-    private final int[] rolls = new int[21];
+    private int[] rolls = new int[21];
     private int roll;
+
+    public BowlingGame() {
+        Arrays.fill(rolls, 0);
+    }
 
     public void roll(int pins) {
 
-        if (roll == 20) {
-            int frameScore = rolls[18] + rolls[19];
-            if (frameScore == 10) {
-                rolls[20] = pins;
-            }
-        } else if (roll < 20) {
-            rolls[roll++] = pins;
-            if (pins == 10) {
-                roll++;
-            }
-        }
+//        if (roll == 20) {
+//            int frameScore = rolls[18] + rolls[19];
+//            if (frameScore == 10) {
+//                rolls[20] = pins;
+//            }
+//        } else if (roll < 20) {
+//            rolls[roll++] = pins;
+//            if (pins == 10 && roll%2!=0) {
+//                roll++;
+//            }
+//        }
+
 
     }
 
-    public void rollMulti(int... pins){
-        for(int p: pins){
-            roll(p);
+    public void rollMulti(int... pins) {
+        int count = 0;
+        for (int p : pins) {
+            rolls[count++] = p;
         }
     }
 
@@ -32,7 +40,7 @@ public class BowlingGame {
 
     public int getScore() {
         int score = 0;
-        for (int rol = 0; rol < rolls.length; rol = rol + 2) {
+        for (int rol = 0; rol < rolls.length - 1; rol = rol + 2) {
 
             int rolScore = rolls[rol];
             int secondRole = rol + 1;
@@ -56,7 +64,7 @@ public class BowlingGame {
                     if (isNextFrameHasStrike(nextFrame1stRol)) {
                         strikeBonus = rolls[nextFrame1stRol] + getNextToNextFrame1stRole(nextToNextFrame1stRoll);
                     } else {
-                        strikeBonus = rolls[nextFrame1stRol] + rolls[nextFrame2ndRol];
+                        strikeBonus = getRoll(nextFrame1stRol) + getRoll(nextFrame2ndRol);
                     }
                     score += strikeBonus;
                 }
@@ -66,6 +74,12 @@ public class BowlingGame {
             }
         }
         return score;
+    }
+
+    private int getRoll(int nextFrame1stRol) {
+        if (nextFrame1stRol >= rolls.length) return 0;
+        else
+            return rolls[nextFrame1stRol];
     }
 
     private int getNextToNextFrame1stRole(int nextToNextFrame1stRoll) {
--- src/test/java/be/techfix/kata/bowling/GameTest.java
+++ src/test/java/be/techfix/kata/bowling/GameTest.java
@@ -12,7 +12,7 @@ public class GameTest {
     @Test
     void should_be_able_to_roll_zero_to_10_pins() {
         BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.roll(0);
+        bowlingGame.rollMulti(0,5);
         bowlingGame.roll(5);
         assertThat(bowlingGame.getRolls()[0]).isEqualTo(0);
         assertThat(bowlingGame.getRolls()[1]).isEqualTo(5);
@@ -21,88 +21,81 @@ public class GameTest {
     @Test
     void should_return_score_of_10_when_user_have_two_rolls_of_7_and_3_while_rest_are_zero() {
         BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.roll(7);
-        bowlingGame.roll(3);
+        bowlingGame.rollMulti(7,3);
         assertThat(bowlingGame.getScore()).isEqualTo(10);
     }
 
     @Test
     void should_return_score_12_for_a_spare_and_roll_of_1() {
         BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.roll(7);
-        bowlingGame.roll(3);//spare
-        bowlingGame.roll(1);
+        bowlingGame.rollMulti(7,3,1);
         assertThat(bowlingGame.getScore()).isEqualTo(12);
     }
 
     @Test
     void should_return_score_20_for_a_spare_and_roll_of_5() {
         BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.roll(5);
-        bowlingGame.roll(5);//spare
-        bowlingGame.roll(5);
+        bowlingGame.rollMulti(5, 5, 5);
+
         assertThat(bowlingGame.getScore()).isEqualTo(20);
     }
 
     @Test
     void should_return_score_16_for_a_strike_followed_by_1_and_2() {
         BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.roll(10);
-        bowlingGame.roll(1);//spare
-        bowlingGame.roll(2);
+        bowlingGame.rollMulti(10,0,1,2);
         assertThat(bowlingGame.getScore()).isEqualTo(16);
     }
 
 
     @Test
-    void should_not_have_frame_of_3_rolls_when_strike_or_spare_doesnot_happens_at_10th_frame() {
-        BowlingGame bowlingGame = new BowlingGame();
-        rollTill10thFrameWithValue(bowlingGame, 3);
-        bowlingGame.roll(3); // strike so eligible for last roll
-        bowlingGame.roll(5);
-        bowlingGame.roll(7);
-
-        assertThat(bowlingGame.getRolls()[20]).isEqualTo(0);
-
-    }
-
-    @Test
-    void should_have_frame_of_3_rolls_when_strike_happens_at_10th_frame() {
-        BowlingGame bowlingGame = new BowlingGame();
-        rollTill10thFrameWithValue(bowlingGame, 3);
-        bowlingGame.roll(10); // strike so eligible for last roll
-        bowlingGame.roll(5);
-        assertThat(bowlingGame.getRolls()[20]).isEqualTo(5);
-    }
-
-    @Test
     void shoudl_have_score_53_for_double_strike_followed_by_5_and_4() {
         BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.rollMulti(10,10,5,4);
+        bowlingGame.rollMulti(10,0, 10,0, 5, 4);
         assertThat(bowlingGame.getScore()).isEqualTo(53);
     }
 
     @Test
     void shoudl_have_score_82_for_triple_strike_followed_by_5_and_4() {
         BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.rollMulti(10,10,10,5,4);
+        bowlingGame.rollMulti(10,0, 10,0, 10,0, 5, 4);
         assertThat(bowlingGame.getScore()).isEqualTo(83);
     }
 
     @Test
     void should_return_score_42_for_a_strike_followed_by_1_and_2_and_a_spare_followed_by_1() {
         BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.rollMulti(5,1,10,1,2,7,1,5,5,1);
+        bowlingGame.rollMulti(5, 1,
+                10, 0,
+                1, 2,
+                7, 1,
+                5, 5,
+                1, 0);
 
         // 5+1{10+1+2+1+2}+7+1+( 5+5+1)+1
         assertThat(bowlingGame.getScore()).isEqualTo(42);
 
     }
 
+    @Test
+    void should_return_181_for_user() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.rollMulti(
+                7, 3
+                , 10, 0
+                , 6, 2
+                , 10, 0
+                , 10, 0
+                , 9, 1
+                , 0, 10
+                , 10, 0
+                , 7, 2
+                , 10, 10, 8
 
-    private void rollTill10thFrameWithValue(BowlingGame bowlingGame, int pins) {
-        bowlingGame.roll(pins);
-        bowlingGame.rollMulti(3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3);
+        );
+        // 5+1{10+1+2+1+2}+7+1+( 5+5+1)+1
+        assertThat(bowlingGame.getScore()).isEqualTo(181);
+
     }
 
 
